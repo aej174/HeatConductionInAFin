45bf6f8418bb891c054dd6ff1235b54c41471031
aej174 <aej174@icloud.com>
2015-05-12 14:16:51 -0400

    Initial Commit

---
+++ HeatConductionInAFin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeatConductionInAFin
+//
+//  Created by Allan Jones on 5/12/15.
+//  Copyright (c) 2015 Allan Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
